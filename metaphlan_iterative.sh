987372ec3e9b43ad466d7f1ef1a006c6ceeedbab
María del Carmen Sánchez <az09-@ciencias.unam.mx>
2022-02-09 14:35:30 -0600

    Create metaphlan_iterative.sh

---
+++ metaphlan_iterative.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# Author: María del Carmen Sánchez
+## Provide:
+## 1) File with the names of the assemblies
+
+FILE=$1
+names=($(cut -f 1 | uniq $FILE))
+for sample in ${names[@]}
+do
+echo "${sample}"
+echo "metaphlan  "${sample}" --input_type  fasta -o  "${sample}.txt""
+metaphlan  "${sample}" --input_type  fasta -o  "${sample}.txt"
+done
